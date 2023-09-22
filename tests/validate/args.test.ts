b60fd73068f48581f6db2fa5ca6a38ae7b700706
ochi <ocos54ce@gmail.com>
2023-09-22 22:00:48 +0900

    validateのテストコード

--- tests/validate/args.test.ts
+++ tests/validate/args.test.ts
@@ -1,11 +1,11 @@
 import { isValidArgs } from "validate/args";
 
 describe("validate args", () => {
-	test("arg length is 3", () => {
+	test("引数が3つの時はtrue", () => {
 		expect(isValidArgs(["node", "index.js", "__URL__"])).toBe(true);
 	});
 
-	test("arg length is not 3", () => {
+	test("引数が4つ以上の時はfalse", () => {
 		expect(isValidArgs(["node", "index.js", "__URL__", "EXTRA_ARG"])).toBe(false);
 	});
 });
