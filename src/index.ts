9f55802aaabc6e309c231f558cade365f26f7e1a
ochi <ocos54ce@gmail.com>
2023-09-26 23:50:07 +0900

    オブジェクトの比較

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,33 @@ if (!isValidArgs(args)) {
 			const previousMetadata: metadataType = JSON.parse(
 				await fs.readFile("snapshots/metadata.json", "utf8"),
 			);
+
+			const mateDateAreEqual = (currentMatadata: any, previousMetadata: any) => {
+				const getKeys = <T extends { [key: string]: unknown }>(obj: T): (keyof T)[] => {
+					return Object.keys(obj);
+				};
+				const currKeys = getKeys(currentMatadata);
+				const prevKeys = getKeys(previousMetadata);
+
+				// プロパティの数が異なる場合
+				if (currKeys.length !== prevKeys.length) {
+					return false;
+				}
+
+				// オブジェクトの型がプリミティブの場合
+				if (typeof currentMatadata !== "object") {
+					return currentMatadata === previousMetadata;
+				}
+
+				for (const key of currKeys) {
+					if (!mateDateAreEqual(currentMatadata[key], previousMetadata[key])) {
+						return false;
+					}
+				}
+
+				return true;
+			};
+			console.log(mateDateAreEqual(metadata, previousMetadata));
 		}
 
 		await fs.writeFile("snapshots/metadata.json", JSON.stringify(metadata));
